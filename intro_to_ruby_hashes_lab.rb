6bdb3291109514573904d1ae7e5082e67fba9245
Janay Bailey <tjanay87@gmail.com>
2019-08-26 20:55:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,10 +10,10 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- :base_hash{ :monopoly_with_second_tier {:monopoly_with_third_tier{}}
+ :base_hash { :monopoly_with_second_tier {:monopoly_with_third_tier{}}
  }
 end
 
 def monopoly_with_fourth_tier
- :base_hash{ :monopoly_with_second_tier {:monopoly_with_third_tier {:monopoly_with_fourth_tier{}}}}
+ :base_hash { :monopoly_with_second_tier {:monopoly_with_third_tier {:monopoly_with_fourth_tier{}}}}
 end
